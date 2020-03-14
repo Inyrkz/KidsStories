e23b1cf30740747c9b959e5732819bd898b60aec
Ini-Abasi Bernard <55382743+Inyrkz@users.noreply.github.com>
2020-03-14 15:55:00 +0100

    Add kid.html file
    Added an html file in static folder, updated code for ks2.py and app.py

--- app.py
+++ app.py
@@ -1,47 +1,29 @@
 '''runnig flask app'''
-import pickle
 import json
 from flask import Flask, jsonify, request
+from flask_restful import Api
 from ks2 import user_age
-
-
-#load model
-MODEL = pickle.load(open('MODEL.pkl', 'rb'))
 
 #app
 app = Flask(__name__)
+api = Api(app)
 
 #routes
 @app.route('/', methods=['POST'])
 def predict():
     '''function to predict'''
-    data = request.get_json(force=True)
-    #convert json to dictionary
-    data = json.loads(data)
-    #get int value from the 'Age' key in the dictionary
-    age = int(data['Age'])
+    #data = request.json.get('Age')
+    message = request.get_json(force=True)
+    #get value from the 'Age' key in the json
+    age = int(message['age'])
     output = user_age(age)
-    #COnvet dataframe to dictionary
-    output = output.set_index('Title').T.to_dict('list')
-    #CONVERT dict to json
-    #result = json.dumps(INFO)
-    result = jsonify(output)
-    output = {'results': result[0]}
-    return output
-    #INFO datafram will be converted to json
-    #INFO = INFO.to_json(orient='records')
-   #get data
-    #MY_AGE = request.get_json(force=True)
-    #convert data into dataframe
-    #data.update((x, [y]) for x, y in data.items())
-    #data_df = pd.DataFrame.from_dict(data)
-    #predictions
-    #result = MODEL(data_df)
-    #send back to browser
-    #output = {'results': int(result[0])}
-    #return data
-    #return jsonify(results=output)
+    response = {
+        'reply' : output
+    } #sends a response to the static html page
+    
+    return jsonify(response) #returning a jsonified response
+    #return jsonify({"Recommended Stories":output})
 
 if __name__ == '__main__':
-    app.run(port = 5000, debug=True)
+    app.run(debug=True)
     
--- ks2.py
+++ ks2.py
@@ -2,7 +2,6 @@
 Building a popularity recommender system for kid's stories based on age
 '''
 #Importing Libraries
-import pickle
 import pandas as pd
 #import numpy as np
 
@@ -11,28 +10,29 @@ def user_age(age):
     Function to get age of user and display books available for that age
     '''
     #Reading the dataset
-    age_int = pd.read_csv('/content/KidsStories/Age_interaction.csv', encoding = 'latin-1')
-    stories = pd.read_csv('/content/KidsStories/Kidstories.csv', encoding = 'latin-1')
+    age_int = pd.read_csv('Age_interaction.csv', encoding='latin-1')
+    stories = pd.read_csv('Kidstories.csv', encoding='latin-1')
     #Let's preview the first few rows of the data
     #print(STORIES.head())
     #AGE_INTERACTIONS.head()
     #Getting recommendation based on age of user
-    books = age_int.loc[age_int.Age == age, :].sort_values('Book_ID', ascending = False)
+    books = age_int.loc[age_int.Age == age, :].sort_values('Book_ID', ascending=False)
     #length = len(age_int.loc[age_int.Age == age, :])
     #print("There are ", length, "books available for age ", age)
     #Getting details of books recommended for that age
-    info = pd.merge(books, stories, on = 'Book_ID')
+    info = pd.merge(books, stories, on='Book_ID')
     info = info.sort_values(['Likes', 'Dislikes'], ascending=[False, True])
     info = info.loc[:, ['Title']]
-    return info
-#while True:
-print('----------------------------')
-print('Age Ranges from 0 - 13 years')
-MY_AGE = int(input('Please enter age:'))
-if MY_AGE > 13:
-    print('Age over expected range')
-else:
-    MODEL = user_age(MY_AGE)
-    print(MODEL)
+    # info = info.to_string(index=False) #convert dataframe to string without the index
+    info = info.to_dict(orient='list') #convert dataframe to dictionary conta
+    if age > 13:
+        ans = 'Age over expected range'
+    else:
+        ans = info
+    return str(ans) #convert output from dictionary to string
 
-pickle.dump(user_age, open("MODEL.pkl", "wb"))
+#while True:
+#Testing the data
+#age = 1
+#print(user_age(int(age)))
+#print(type(user_age(int(age))))
